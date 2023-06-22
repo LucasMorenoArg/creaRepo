37220e8d0dd19d91a8ba829837586ba4436406b0
LucasMorenoArg <lucas.moreno.ragni@mi.unc.edu.ar>
2023-06-22 15:46:18 -0300

    Tp3

---
+++ TercerTP/src/test/java/com/example/vates/TercerTP/TercerTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vates.TercerTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TercerTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
