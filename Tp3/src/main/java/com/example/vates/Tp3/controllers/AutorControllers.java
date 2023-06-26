b6bebfd84fd5eb948eed77f2b483ec562e892224
lucas moreno <lucas.moreno.ragni@mi.unc.edu.ar>
2023-06-26 11:22:58 -0300

    ABM de Autor

--- Tp3/src/main/java/com/example/vates/Tp3/controllers/AutorControllers.java
+++ Tp3/src/main/java/com/example/vates/Tp3/controllers/AutorControllers.java
@@ -2,10 +2,9 @@ package com.example.vates.Tp3.controllers;
 
 import com.example.vates.Tp3.entities.Autor;
 import com.example.vates.Tp3.service.ServicioAutor;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -19,9 +18,29 @@ public class AutorControllers {
     public Collection<Autor> consultaAutor(){
        return servicioAutor.consultarTodos();
     }
+
     @GetMapping("/buscarxId/{id}")
     public Autor consultaxId(@PathVariable int id){
         return servicioAutor.buscarxId(id);
+    }
+
+    @PutMapping("/crearAutor")
+    public boolean crearAutor(@RequestBody Autor autor){
+        servicioAutor.crearAutor(autor);
+        return true;
+    }
+
+    @PutMapping("/actualizarAutor")
+    public boolean actualizar(@RequestBody Autor autor){
+        servicioAutor.actualizarAutor(autor);
+        return true;
+    }
+
+
+    @DeleteMapping("/borrarAutor/{id}")
+    public boolean borrar(@PathVariable int id){
+        servicioAutor. eliminarAutor(id);
+        return true;
     }
 
 }
--- Tp3/src/main/java/com/example/vates/Tp3/service/ServicioAutor.java
+++ Tp3/src/main/java/com/example/vates/Tp3/service/ServicioAutor.java
@@ -4,12 +4,14 @@ import com.example.vates.Tp3.entities.Autor;
 import com.example.vates.Tp3.entities.Libro;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
 @Service
 public class ServicioAutor {
+
     @PersistenceContext
     private EntityManager emAutor;
 
@@ -21,6 +23,20 @@ public class ServicioAutor {
         return emAutor.find(Autor.class, id);
     }
 
+    @Transactional
+    public void crearAutor(Autor autor){
+        emAutor.persist(autor);
+    }
 
+    @Transactional
+    public void actualizarAutor(Autor autor){
+        emAutor.merge(autor);
+    }
+
+    @Transactional
+    public void eliminarAutor(int id){
+        Autor borrarAutor = buscarxId(id);
+        emAutor.remove(borrarAutor);
+    }
 
 }
